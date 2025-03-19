45544fd8b121c29c468a2ad0b979e32f4ad6239f
nakashima1231 <gabrielmnakashima2@gmail.com>
2025-03-18 22:09:41 -0300

    Add files via upload

---
+++ ex3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int contador = 1; 
+    int lado1;
+    int lado2;
+    int lado3;
+    int hipo = 0;
+    int hipo2 = 0;
+    
+    while(contador != -1) {
+        printf("Digite o valor do lado 1: ");
+        scanf("%d", &lado1);
+        printf("Digite o valor do lado 2: ");
+        scanf("%d", &lado2);
+        printf("Digite o valor do lado 3: ");
+        scanf("%d", &lado3);
+        
+        if (lado1 > lado 2) {
+            if(lado1 > lado 3) {
+               
+            }
+        }
+        
+        
+        
+        
+        printf("quer continuar?(digite -1 caso queira parar): ");
+        scanf("%d", &contador);
+    }
+    return 0;
+}
